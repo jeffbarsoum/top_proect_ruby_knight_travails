c6d42ae24608d2a826e6ea14fedc56f8922d3f94
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-07-31 08:48:38 -0400

    Add time_discovered and time_finished for DFS algorithm

--- lib/node.rb
+++ lib/node.rb
@@ -4,7 +4,7 @@
 class Node
   include Comparable
   # setters and getters for "value" and "next_node"
-  attr_accessor :distance, :predecessor
+  attr_accessor :distance, :predecessor, :time_discovered, :time_finished
 
   def <=>(other)
     return 0 if distance.nil? && other&.distance.nil?
@@ -17,9 +17,16 @@ class Node
   def initialize(distance = nil, predecessor = nil)
     self.distance = distance
     self.predecessor = predecessor
+    self.time_discovered = nil
+    self.time_finished = nil
   end
 
   def to_s
-    "[ { distance: #{distance},  predecessor: #{predecessor} }"
+    param_label = time_discovered ? 'discovered / finsihed' : 'distance'
+    param = time_discovered ? "#{time_discovered} / #{time_finished}" : distance
+    <<-STRING
+    { distance: #{distance},  predecessor: #{predecessor} }
+    [  #{param_label} = #{param} ]
+    STRING
   end
 end
